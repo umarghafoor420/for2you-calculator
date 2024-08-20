aa5e054b49257ed54cc05fc1299643532e361b28
Umar Ghafoor <164578000+umarghafoor420@users.noreply.github.com>
2024-08-21 02:52:40 +0500

    Add files via upload
    This is a simple calculator made with typescript and javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { massage: "Enter firstnumber", type: "Number", name: "firstnumber" },
+    { massage: "Enter secondnumber", type: "Number", name: "secondnumber" },
+    { massage: "Selet one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Subtraction", "Multipilication", "Division", "ADDITION"], }
+]);
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstnumber + asnwer.secondnumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstnumber - asnwer.secondnumber);
+}
+else if (asnwer.operator === "Multipilication") {
+    console.log(asnwer.firstnumber * asnwer.secondnumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstnumber / asnwer.secondnumber);
+}
+else {
+    console.log("please select valide operator");
+}
